01aa59e89b139a43f3c3a6573e1d3af0e6233aa0
Kamran <311.kamran@gmail.com>
2023-05-01 09:22:31 +0400

    Generate Code

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-let DownloadImgs = ["images/agac.jpg", "images/agac2.jpg", "images/agac3.jpg", "images/gobelek.jpg", "images/kirpi.jpg", "images/qoza.jpg", "images/sabalid.jpg", "images/umbrella.jpg", "images/yarpaq.jpg", "images/gobelek2.jpg",    "images/agac.jpg", "images/agac2.jpg", "images/agac3.jpg", "images/gobelek.jpg", "images/kirpi.jpg", "images/qoza.jpg", "images/sabalid.jpg", "images/umbrella.jpg", "images/yarpaq.jpg", "images/gobelek2.jpg"];
+let DownloadImgs = ["images/agac.jpg", "images/agac2.jpg", "images/agac3.jpg", "images/gobelek.jpg", "images/kirpi.jpg", "images/qoza.jpg", "images/sabalid.jpg", "images/umbrella.jpg", "images/yarpaq.jpg", "images/gobelek2.jpg", "images/agac.jpg", "images/agac2.jpg", "images/agac3.jpg", "images/gobelek.jpg", "images/kirpi.jpg", "images/qoza.jpg", "images/sabalid.jpg", "images/umbrella.jpg", "images/yarpaq.jpg", "images/gobelek2.jpg"];
 
 
 let Array = [];
 
 
-
 function StartGame() {
-
     let element = document.getElementById('mySection');
     let content = '';
 
     for (let i = 0; i < 4; i++) {
-
         content += "<section>";
         let subArray = [];
 
         for (let k = 0; k < 5; k++) {
-
             let number = getRandom(DownloadImgs.length);
             let choiceImg = DownloadImgs[number];
 
@@ -26,18 +22,12 @@ function StartGame() {
 
             const index = DownloadImgs.indexOf(choiceImg);
             DownloadImgs.splice(index, 1);
-
         }
-
         Array.push(subArray);
         content += "</section>";
-
     }
-
     element.innerHTML = content;
-
 }
-
 
 
 console.log(Array);
@@ -50,33 +40,29 @@ let saygac = 0;
 
 function ShowImage(id) {
     let element = document.getElementById(id);
-    let i = 0;
-    let k = 0;
     let data = String(id).split("-");
-    i = data[0];
-    k = data[1];
+    let i = data[0];
+    let k = data[1];
 
     element.src = Array[i][k];
     IkiliArray.push(element.src);
 
-
     // Check Card
-    
+    ++saygac;
+
     if (IkiliArray.length == 2) {
         if (IkiliArray[0] == IkiliArray[1]) {
             element.src = Array[i][k];
             IkiliArray = [];
             count++;
+            saygac=0;
         }
     }
-    
     else if (IkiliArray.length > 2) {
         StartGame();
         IkiliArray = [];
         count = 0;
     }
-    
-    ++saygac;
 
     if (count == 10) {
         setTimeout(() => {
@@ -85,20 +71,20 @@ function ShowImage(id) {
             count = 0;
         }, 1000);
     }
-
     if (saygac == 16) {
-        alert("Game Over");
-        StartGame();
-        saygac = 0;
+        setTimeout(() => {
+            alert("Game Over");
+            StartGame();
+            saygac = 0;
+        }, 1000);
     }
-}
 
+    console.log(saygac);
+}
 
 
 function getRandom(max) {
     return Math.floor(Math.random() * max);
 }
-
-
 
 StartGame();
